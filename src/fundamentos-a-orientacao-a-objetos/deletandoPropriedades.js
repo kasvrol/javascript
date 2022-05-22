a413db7df35d53498640f030fe9b3a29372b2ca6
kasvrol <dovalekarolina@gmail.com>
2022-05-22 20:54:04 -0300

    feat: arquivo que demonstra a exclusão de propriedades de um objeto

--- src/fundamentos-a-orientacao-a-objetos/deletandoPropriedades.js
+++ src/fundamentos-a-orientacao-a-objetos/deletandoPropriedades.js
@@ -9,11 +9,19 @@ const objPersonagem = {
     status: "desaparecido",
 };
 
-console.log(objPersonagem.aliado); //undefined
+console.log(objPersonagem.aliado);
+
+delete objPersonagem.aliado;
+
+console.log(objPersonagem.aliado);
+
+//---------------------------------------------------------------
+
+console.log(objPersonagem.aliado);
 console.log(objPersonagem.status);
 
 delete objPersonagem.aliado;
 delete objPersonagem["status"];
 
-console.log(objPersonagem.aliado); //undefined
+console.log(objPersonagem.aliado);
 console.log(objPersonagem.status);
